5957fcf40337c497c8b900dc95581c99564bfaf9
paulcharachon112 <84780100+paulcharachon112@users.noreply.github.com>
2021-05-25 15:24:28 +0200

    Create Scénario_1.py

---
+++ Scénario_1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#  -*- coding: utf-8 -*-
+
+# Date: Fri 25 MAY 2021 15:00:50 UTC+1
+# Author: Paul CHARACHON
+# Description: Scénario 1: Faire un arc de cercle
+# avec un panneau solaire, en s'arretant toute les 0.5 secondes
+# Last Modified : Tue 25 MAY 2021 18:50
+# Python Version 3.9
+
+import 
