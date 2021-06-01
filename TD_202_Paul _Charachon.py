125179e5b03ad15a0f8d2767513c03e1106f83cf
paulcharachon112 <84780100+paulcharachon112@users.noreply.github.com>
2021-06-01 14:21:09 +0200

    Create TD_202_Paul _Charachon.py

---
+++ TD_202_Paul _Charachon.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#  -*- coding: utf-8 -*-
+
+# Date: Fri 25 MAY 2021 15:00:50 UTC+1
+# Author: Paul CHARACHON
+# Description: Imprimer les 100 premiers nombres sans
+# utiliser de for ou de while loop
+# Last Modified : Tue 31 MAY 2021 18:50
+# Python Version 3.9
+def 100_no_loops():
+""" on va chercher le nombre ASCII de e , qui est 101, puis on print chaque valeur 
+jusqua 100 avec une fonction range"""
+
+    [*range(True,ord("e"))]
