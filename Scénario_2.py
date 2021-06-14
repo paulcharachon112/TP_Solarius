b0a7d39b4df449188314227ca0089296256e90ec
paulcharachon112 <84780100+paulcharachon112@users.noreply.github.com>
2021-06-15 00:20:34 +0200

    Update Scénario_2.py
    FInal version ( normally)

--- Scénario_2.py
+++ Scénario_2.py
@@ -3,49 +3,74 @@
 
 # Date: Fri 25 MAY 2021 15:00:50 UTC+1
 # Author: Paul CHARACHON
-# Description: Scénario 1: Faire un arc de cercle
-# avec un panneau solaire, en s'arretant toute les 0.5 secondes
-# Last Modified : Tue 25 MAY 2021 18:50
+# Description: Scénario 2: Suivre la source de lumière
+# à l'aide d'un seul capteur de lumière
+# Last Modified : Tue 31 MAY 2021 18:50
 # Python Version 3.9
 # Used https://learn.adafruit.com/adafruit-16-channel-servo-driver-with-raspberry-pi/using-the-adafruit-library
-import time
 from adafruit_motor import servo
 from adafruit_servokit import ServoKit
+from adafruit_circuitplayground import cp
+from time import time, sleep
 kit = ServoKit(channels=13)
-"""defining the total number of pins on my microcontroller """
-def servo_horizontale(position_start, position_final, sens):
 
-""" defining the horizontal movement"""
 
-while position < position_final:
-    position = position_start
-    kit.servo[0].angle = position_start
-    position = position + int(sens) * 5
-""" updating the new value of the angle of the servo"""
+def lights():
+    """ randomimizing movements to comprare 4 light values, and finds the biggest value"""
+    """collecting light value for up"""
+    servo_verticale(int(position_verticale) + 10)
+    lights_up = cp.light  # collecting light data
+    """collecting light value for down; -10 for other side (+ -10 to return to orignial spot) """
+    servo_verticale(int(position_verticale) - 20)
+    lights_down = cp.light  # collecting light data
+    servo_verticale(int(position_verticale))  # returning to original position
+    """collecting light value for gauche"""
+    servo_horizontale(int(position_horizontale) + 10)
+    lights_gauche = cp.light  # collecting light data
+    """collecting light value for droite; -10 for other side (+-10 to return to orignial spot) """
+    servo_horizontale(int(position_horizontale) - 10)
+    lights_droite = cp.light  # collecting light data
+    # returning to original position
+    servo_horizontale(int(position_horizontale))
+    """comparing all of the values and finding the biggest one, storing it in a new variable"""
+    light_values = {'up': lights_up, 'down': lights_down,
+                    'gauche': lights_gauche, 'droite': lights_droite}
+    strongest_light = max(light_values, key=light_values.get)
+
+
+def servo_horizontale(position):
+    """ defining the horizontal left movement, takes the new value at the start
+    of each iteration"""
+
+    kit.servo[0].angle = position
     time.sleep(0, 5)
-    break
 
-def servo_verticale(position_start, position_final, sens):
 
-""" defining the horizontal movement"""
+def servo_verticale(position):
+    """ defining the vertial up movement, takes the new value at the start
+    of each iteration"""
 
-while position < position_final:
-    position_2 = position_start
-    kit.servo[0].angle = position_2
-    position_2 = position_2 + int(sens) * 5
-    """ updating the new value of the angle of the servo"""
+    kit.servo[1].angle = position
     time.sleep(0, 5)
-    break
 
 
-def scénario_1():
-	""" definfing the first scenario"""
-    While position_2 < 90:
-    """ sets a condition for vertical position and also sunrise"""
-        servo_horizontale(0, 90, 1)
-        servo_verticale(0, 90, 1)
-
-    While position_2 > 0:
-    """sundown = afternoon"""
-        servo_horizontale(90, 180, 1)
-        servo_verticale(90, 0, -1)
+def scénario_2():
+    """ defining the second scenario, every 2 seconds it searches for the best 
+    value for light, then turns towards that light"""
+    position_horizontale = 10
+    position_verticale = 10
+    while True:
+        lights()
+        sleep(2)
+        if strongest_light = 'up':
+            servo_verticale(position_verticale + 10)
+            position_verticale = position_verticale + 10
+        elif strongest_light = 'down':
+            servo_verticale(position_verticale - 10)
+            position_verticale = position_verticale - 10
+        elif strongest_light = 'gauche':
+            servo_horizontale(position_horizontale + 10)
+            position_horizontale = position_horizontale + 10
+        elif strongest_light = 'droite':
+            servo_horizontale(position_horizontale - 10)
+            position_horizontale = position_horizontale - 10
